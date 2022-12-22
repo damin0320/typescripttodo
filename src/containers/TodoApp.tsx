3888eadce265a7d73d1867cc89e7e12477a436f1
damin <damin0320@gmail.com>
2022-12-22 17:41:35 +0900

    ADD : TodoApp 연습

--- src/containers/TodoApp.tsx
+++ src/containers/TodoApp.tsx
@@ -7,24 +7,24 @@ import { RootState } from '../modules';
 import { addTodo, removeTodo, toggleTodo } from '../modules/todos';
 
 const TodoApp = () => {
-    const todos = useSelector((state: RootState) => state.todos)
-    const dispatch = useDispatch();
+  const todos = useSelector((state:RootState) => state.todos)
+  const dispatch = useDispatch();
 
-    const onInsert = (text: string) => {
-        dispatch(addTodo(text))
-    }
-    const onToggle = (id: number) => {
-        dispatch(toggleTodo(id))
-    }
-    const onRemove = (id: number) => {
-        dispatch(removeTodo(id))
-    }
-    return (
-        <div>
-            <TodoInsert onInsert={onInsert}/>
-            <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove}/>
-        </div>
-    );
-};
+  const onInsert = (text:string) => {
+    dispatch(addTodo(text))
+  }
+  const onToggle = (id : number) => {
+    dispatch(toggleTodo(id))
+  }
+  const onRemove = (id : number) => {
+    dispatch(removeTodo(id))
+  }
+  return(
+    <div>
+      <TodoInsert onInsert={onInsert}/>
+      <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove}/>
+    </div>
+  )
+}
 
 export default TodoApp;
