6a1879f18c94428d4144e449faeace779702959e
damin <damin0320@gmail.com>
2022-12-22 17:36:16 +0900

    ADD : TodoList 연습

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,19 +4,20 @@ import TodoItem from './TodoItem';
 
 // props 타입지정
 type TodolistProps = {
-    todos: Todo[];
-    onToggle:(id:number) => void;
-    onRemove:(id:number) => void;
+  todos: Todo[];
+  onToggle:(id:number) => void;
+  onremove:(id:number) => void;
 }
-const TodoList = ({todos, onToggle, onRemove}: TodolistProps) => {
-    if(todos.length === 0 ) return <div>등록한 항목이 없습니다.</div>
-    return (
-        <ul>
-            {todos.map(todo=>(
-                <TodoItem todo={todo} onToggle={onToggle} onRemove={onRemove} key={todo.id}/>
-            ))}
-        </ul>
-    );
+const TodoList = ({todos, onToggle, onRemove} : TodolistProps) => {
+  if(todos.length === 0)
+  return <div>등록한 todoList가 없습니다.</div>
+  return (
+    <ul>
+      {todos.map(todo => (
+        <TodoItem todo={todo} onToggle={onToggle} onRemove={onRemove} key={todo.id}/>
+      ))}
+    </ul>
+  );
 };
 
 export default TodoList;
