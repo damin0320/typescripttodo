72545f4b01e936e6b98b8e3bb82972374ddce7fb
damin <damin0320@gmail.com>
2022-12-22 16:47:19 +0900

    ADD : reducer 연습

--- src/modules/todos.ts
+++ src/modules/todos.ts
@@ -2,26 +2,31 @@
 // 할 일 추가, 할 일 제거, 할 일 체크
 
 // 액션타입 선언
-const ADD_TODO = 'todos/ADD_TODO' as const;
+const ADD_TODO = 'todo/ADD_TODO' as const;
 const TOGGLE_TODO = 'todos/TOGGLE_TODO' as const;
 const REMOVE_TODO = 'todos/REMOVE_TODO' as const;
 
+
+// id값 설정
 let nextId = 1;
+
 // 액션 생성 함수
-export const addTodo = (text: string) => ({
-    type: ADD_TODO,
-    payload: {
-        id: nextId++,
-        text
-    }
+export const addTodo = (text : string) => ({
+  type : ADD_TODO,
+  payload : {
+    id : nextId++, 
+    text
+  }
 })
-export const toggleTodo = (id:number) => ({
-    type: TOGGLE_TODO,
-    payload: id
+
+export const toggleTodo = (id : number) => ({
+  type : TOGGLE_TODO,
+  payload : id
 })
-export const removeTodo = (id:number) => ({
-    type: REMOVE_TODO,
-    payload: id
+
+export const removeTodo = (id : number) => ({
+  type : REMOVE_TODO,
+  payload : id
 })
 
 // 액션 객체들에 대한 타입 작성
@@ -32,37 +37,37 @@ type TodosAction =
 
 // 상태에서 사용할 할일 항목 데이터 정리
 export type Todo = {
-    id: number;
-    text: string;
-    done: boolean;
+  id : number;
+  text: string;
+  done: boolean;
 }
 
 // 이 모듈에서 관리할 상태 타입 작성
 export type TodoState = Todo[]
 
 // 초기상태 선언
-const initialState: TodoState = [];
+const initialState : TodoState = [];
 
 // 리듀서 작성하기
 function todos(
-    state: TodoState = initialState,
-    action: TodosAction
+  state: TodoState = initialState,
+  action : TodosAction
 ) : TodoState {
-    switch(action.type) {
-        case ADD_TODO:
-            return state.concat({
-                id:action.payload.id,
-                text: action.payload.text,
-                done: false
-            });
-        case TOGGLE_TODO:
-            return state.map(todo=>
-                todo.id === action.payload ? {...todo, done: !todo.done } : todo
-                )
-        case REMOVE_TODO:
-            return state.filter(todo => todo.id !== action.payload);
+  switch(action.type) {
+    case ADD_TODO:
+      return state.concat({
+        id: action.payload.id,
+        text:action.payload.text,
+        done:false
+      });
+      case TOGGLE_TODO:
+        return state.map(todo => todo.id === action.payload ? {...todo, done : !todo.done} : todo)
+      case REMOVE_TODO:
+        return state.filter(todo => todo.id !== action.payload);
         default:
-            return state
-    }
+          return state
+
+  }
 }
+
 export default todos;
